cb9804bc8c77b4c41e75addb87f7606515931592
Sam Denton <samwyse@gmail.com>
2022-10-28 16:06:43 -0500

    add something I wrote a few years ago
    This adds an argument group to an argparse parser and returns a function to create a CSV Dialect object from the Namespace object built from parsing a command line.

---
+++ argparse_csv.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python
+
+"""Add documentation here."""
+
+# Insure maximum compatibility between Python 2 and 3
+from __future__ import absolute_import, division, print_function
+
+try:
+    basestring
+except NameError:
+    basestring = str
+
+# Metadate...
+__author__ = "Samuel T. Denton, III <sam.denton@dell.com>"
+__contributors__ = []
+__copyright__ = "Copyright 2021 Samuel T. Denton, III"
+__version__ = '0.9'
+
+# Declutter our namespace
+__all__ = ['add_csv_argments']
+
+# Python standard libraries
+import argparse, csv
+
+def singlechar(s):
+    try:
+        if len(s) == 1:
+            return s
+    except:
+        pass
+    raise ValueError('must be a single character')
+
+quoting_choices = {
+    'minimal': csv.QUOTE_MINIMAL,
+    'all': csv.QUOTE_ALL,
+    'nonnumeric': csv.QUOTE_NONNUMERIC,
+    'none': csv.QUOTE_NONE,
+    }
+
+is_bool = {'action': 'store_true'}
+is_char = {'type': singlechar, 'metavar': 'CHAR'}
+is_string = {'type': str, 'metavar': 'STR'}
+is_quoting = {'choices': quoting_choices, 'type': lambda s: quoting_choices[s]}
+
+dialect_attributes = (
+    ('delimiter', is_char),
+    ('quotechar', is_char),
+    ('escapechar', is_char),
+    ('doublequote', is_bool),
+    ('skipinitialspace', is_bool),
+    ('lineterminator', is_string),
+    ('quoting', is_quoting),
+    )
+
+def add_csv_argments(parser, title=None, description=None, prefix='csv'):
+    """Add an argument group to the given parser, using the given title
+    and description.  Return a function to create a CSV Dialect object from
+the Namespace object built from parsing a command line:
+
+>>> parser = argparse.ArgumentParser()
+
+>>> dialect_builder = add_csv_argments(parser, title="CSV format",
+...     description="Specify the details of the CSV file format.")
+
+>>> ns = parser.parse_args([])
+>>> dialect = dialect_builder(ns)
+
+>>> dialect.lineterminator
+'\\r\\n'
+>>> dialect.skipinitialspace
+False
+>>> dialect.quoting == csv.QUOTE_MINIMAL
+True
+>>> dialect.delimiter
+','
+>>> dialect.doublequote
+False
+
+"""
+
+    new_group = parser.add_argument_group(title=title, description=description)
+    if prefix:
+        arg_prefix = '--' + prefix + '-'
+        attr_prefix = prefix + '_'
+    else:
+        arg_prefix = '--'
+        attr_prefix = ''
+        
+    for name, kwargs in dialect_attributes:
+        new_group.add_argument(arg_prefix+name, **kwargs)
+
+    def builder(args):
+        """Build a CSV Dialect from an argparse Namespace"""
+        class dialect(csv.excel):
+            pass
+
+        for key, _ in dialect_attributes:
+            attr_name = attr_prefix + key
+            attr_value = getattr(args, attr_name)
+            if attr_value is not None:
+                setattr(dialect, key, attr_value)
+        return dialect
+    return builder
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
