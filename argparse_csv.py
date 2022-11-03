f3e1b2d14f99efb196382d041577ef827e265c4c
Sam Denton <samwyse@gmail.com>
2022-11-03 14:08:03 -0500

    modified to monkey-patch argparse.AgumentParser

--- argparse_csv.py
+++ argparse_csv.py
@@ -1,14 +1,33 @@
 #! /usr/bin/env python
 
-"""Command line arguments to define CSV dialects."""
+"""How to use this...
+
+# Use "exit_on_error=False" to make doctest happier.
+>>> parser = argparse.ArgumentParser(exit_on_error=False)
+
+>>> csv_group = parser.add_csv_group(
+...     title="CSV format",
+...     description="Specify the details of the CSV file format.")
+
+>>> ns = parser.parse_args('--delimiter / --quoting none'.split())
+>>> argparser_dialect = csv_group.get_dialect(ns)
+>>> csvout = csv.writer(sys.stdout, dialect=argparser_dialect)
+
+>>> ns = parser.parse_args('--delimiter / --quoting all'.split())
+>>> argparser_dialect = csv_group.get_dialect(ns)
+>>> csvout = csv.writer(sys.stdout, dialect=argparser_dialect)
+Traceback (most recent call last):
+    ...
+TypeError: quotechar must be set if quoting enabled
+
+>>> ns = parser.parse_args('--delimiter / --quoting all --quotechar ='.split())
+>>> argparser_dialect = csv_group.get_dialect(ns)
+>>> csvout = csv.writer(sys.stdout, dialect=argparser_dialect)
+
+"""
 
 # Insure maximum compatibility between Python 2 and 3
 from __future__ import absolute_import, division, print_function
-
-try:
-    basestring
-except NameError:
-    basestring = str
 
 # Metadate...
 __author__ = "Samuel T. Denton, III <sam.denton@dell.com>"
@@ -17,13 +36,63 @@ __copyright__ = "Copyright 2021 Samuel T. Denton, III"
 __version__ = '0.9'
 
 # Declutter our namespace
-__all__ = ['add_csv_argments']
+__all__ = []
 
 # Python standard libraries
-import argparse, csv
+import argparse, csv, sys
+
+# https://bugs.python.org/issue34188#msg323681
+class StoreMapping(argparse._StoreAction):
+    """
+    Argparse action that interprets the `choices` argument as a dict
+    mapping the user-specified choices values to the resulting option
+    values.
+    
+    >>> adict = {'rock': 4, 'paper': 5, 'scissors': 8}
+    >>> parser = argparse.ArgumentParser()
+    >>> parser.add_argument('--foo', action=StoreMapping, choices=adict)  # doctest: +ELLIPSIS
+    StoreMapping(option_strings=['--foo'], dest='foo', ...)
+    >>> parser.parse_args('--foo rock'.split())
+    Namespace(foo=4)
+    """
+    def __init__(self, *args, choices, **kwargs):
+        super().__init__(*args, choices=choices.keys(), **kwargs)
+        self.mapping = choices
+    def __call__(self, parser, namespace, value, option_string=None):
+        setattr(namespace, self.dest, self.mapping[value])
+
+class AppendMapping(StoreMapping):
+    """
+    >>> adict = {'rock': 4, 'paper': 5, 'scissors': 8}
+    >>> parser = argparse.ArgumentParser()
+    >>> parser.add_argument('--foo', action=AppendMapping, choices=adict)  # doctest: +ELLIPSIS
+    AppendMapping(option_strings=['--foo'], dest='foo', ...)
+    >>> parser.parse_args('--foo rock --foo scissors'.split())
+    Namespace(foo=[4, 8])
+    """
+    def __call__(self, parser, namespace, value, option_string=None):
+        items = getattr(namespace, self.dest, None)
+        items = argparse._copy_items(items)
+        items.append(self.mapping[value])
+        setattr(namespace, self.dest, items)
+
+class ExtendMapping(StoreMapping):
+    """
+    >>> adict = {'rock': 4, 'paper': 5, 'scissors': 8}
+    >>> parser = argparse.ArgumentParser()
+    >>> parser.add_argument('--foo', action=ExtendMapping, choices=adict, nargs='+')  # doctest: +ELLIPSIS
+    ExtendMapping(option_strings=['--foo'], dest='foo', nargs='+', ...)
+    >>> parser.parse_args(["--foo", "rock", "--foo", "paper", "scissors", "rock"])
+    Namespace(foo=[4, 5, 8, 4])
+    """
+    def __call__(self, parser, namespace, values, option_string=None):
+        items = getattr(namespace, self.dest, None)
+        items = argparse._copy_items(items)
+        items.extend(self.mapping[value] for value in values)
+        setattr(namespace, self.dest, items)
 
 def singlechar(s):
-    """Argument must be a single character."""
+    """Validate that 's' is a single character."""
     try:
         if len(s) == 1:
             return s
@@ -31,16 +100,17 @@ def singlechar(s):
         pass
     raise ValueError('must be a single character')
 
-# Allowed ways of quoting
 quoting_choices = {
     name[6:].lower(): getattr(csv, name)
-    for name in dir(csv) if name.startswith('QUOTE_')}
+    for name in dir(csv) if name.startswith('QUOTE_')
+    }
 
-# The various types of values in a Dialect
-is_bool = {'action': 'store_true'}
+try:
+    is_bool = {'action': argparse.BooleanOptionalAction}
+except AttributeError:
+    is_bool = {'action': 'store_true'}
+
 is_char = {'type': singlechar, 'metavar': 'CHAR'}
-is_string = {'type': str, 'metavar': 'STR'}
-is_quoting = {'choices': quoting_choices, 'type': quoting_choices.get}
 
 dialect_attributes = (
     ('delimiter', is_char),
@@ -48,60 +118,53 @@ dialect_attributes = (
     ('escapechar', is_char),
     ('doublequote', is_bool),
     ('skipinitialspace', is_bool),
-    ('lineterminator', is_string),
-    ('quoting', is_quoting),
+    ('lineterminator', {'metavar': 'STR'}),
+    ('quoting', {'action': StoreMapping, 'choices': quoting_choices}),
     )
 
-def add_csv_argments(parser, title=None, description=None, prefix='csv'):
+def add_csv_group(container, *args, **kwargs):
     """Add an argument group to the given parser, using the given title
-    and description.  Return a function to create a CSV Dialect object from
-the Namespace object built from parsing a command line:
+    and description.  The returned group has a property 'get_dialect'
+    that's a function to create a CSV Dialect object from a Namespace
+    object built from parsing a command line."""
 
->>> parser = argparse.ArgumentParser()
+    group = container.add_argument_group(
+        title=kwargs.get('title'),
+        description=kwargs.get('description'))
 
->>> dialect_builder = add_csv_argments(parser, title="CSV format",
-...     description="Specify the details of the CSV file format.")
-
->>> ns = parser.parse_args([])
->>> dialect = dialect_builder(ns)
-
->>> dialect.lineterminator
-'\\r\\n'
->>> dialect.skipinitialspace
-False
->>> dialect.quoting == csv.QUOTE_MINIMAL
-True
->>> dialect.delimiter
-','
->>> dialect.doublequote
-False
-
-"""
-
-    new_group = parser.add_argument_group(title=title, description=description)
+    prefix = kwargs.get('prefix')
     if prefix:
         arg_prefix = '--' + prefix + '-'
         attr_prefix = prefix + '_'
     else:
         arg_prefix = '--'
         attr_prefix = ''
-        
-    for name, kwargs in dialect_attributes:
-        new_group.add_argument(arg_prefix+name, **kwargs)
 
-    def builder(args):
+    for name, kwds in dialect_attributes:
+        group.add_argument(arg_prefix+name, **kwds)
+
+    def get_dialect(ns):
         """Build a CSV Dialect from an argparse Namespace"""
-        class dialect(csv.excel):
-            pass
-
+        class dialect(csv.Dialect):
+            _name = "argparser_dialect"
+            lineterminator = '\r\n'
+            quoting = csv.QUOTE_NONE
         for key, _ in dialect_attributes:
             attr_name = attr_prefix + key
-            attr_value = getattr(args, attr_name)
+            attr_value = getattr(ns, attr_name)
             if attr_value is not None:
                 setattr(dialect, key, attr_value)
         return dialect
-    return builder
+
+    group.get_dialect = get_dialect
+    return group
+
+# monkey-patch argparse to make 'add_csv_group' look native
+argparse._ActionsContainer.add_csv_group = add_csv_group
+del add_csv_group
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+
